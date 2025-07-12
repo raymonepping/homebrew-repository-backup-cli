bc9ce2eea2c6fbc9d83ccba1f7e5995b058e3c95
Raymon Epping <your@email.com>
2025-07-12 08:45:46 +0200

    12/07/2025 - Updated configuration and fixed bugs

--- Formula/repository-backup-cli.rb
+++ Formula/repository-backup-cli.rb
@@ -1,5 +1,5 @@
 class RepositoryBackupCli < Formula
-  desc "Modular, dryrun-safe, self-healing backup CLI with integrity checks and Markdown summaries"
+  desc "CLI toolkit for backing up GitHub repositories with tagging, ignore rules, and recovery"
   homepage "https://github.com/raymonepping/repository_backup_cli"
   url "https://github.com/raymonepping/repository_backup_cli/archive/refs/tags/v1.0.2.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
@@ -10,8 +10,8 @@ class RepositoryBackupCli < Formula
 
   def install
     bin.install "bin/repository_backup.sh" => "repository_backup"
-    pkgshare.install "lib", "docs", "examples" if File.directory?("lib")
-    doc.install "README.md" if File.exist?("README.md")
+    pkgshare.install "core", "templates"
+    doc.install "README.md"
   end
 
   def caveats
@@ -19,15 +19,12 @@ class RepositoryBackupCli < Formula
       To get started, run:
         repository_backup --help
 
-      The library and examples (if present) are located in:
-        #{opt_pkgshare}
-
-      If needed in scripts:
-        export REPOSITORY_BACKUP_HOME=#{opt_pkgshare}
+      If you use templates or configs from the repo, export:
+        export REPO_BACKUP_HOME=#{opt_pkgshare}
     EOS
   end
 
   test do
-    assert_match "repository_backup", shell_output("#{bin}/repository_backup --version 2>&1", 1)
+    assert_match "repository_backup", shell_output("#{bin}/repository_backup --version")
   end
 end

76eae9b25a9dc47c2399ede97e5cc151cee495b8
Raymon Epping <your@email.com>
2025-07-16 08:01:51 +0200

    chore(formula): update to v1.5.3

--- Formula/repository-backup-cli.rb
+++ Formula/repository-backup-cli.rb
@@ -1,11 +1,11 @@
 class RepositoryBackupCli < Formula
   desc "CLI toolkit for backing up GitHub repositories with tagging, ignore rules, and recovery"
   homepage "https://github.com/raymonepping/repository_backup_cli"
-  url "https://github.com/raymonepping/homebrew-repository-backup-cli/archive/refs/tags/v1.5.2.tar.gz"
+  url "https://github.com/raymonepping/homebrew-repository-backup-cli/archive/refs/tags/v1.5.3.tar.gz"
 
-  sha256 "2186d3a097c3af585634665a310408147f8587fccc4136c6c8871d9a339c8e47"
+  sha256 "5d954c95432d88e8f478448452a14e39c35d8b2617250936adc913c842f051c7"
   license "MIT"
-  version "1.5.2"
+  version "1.5.3"
 
   depends_on "bash"
 
